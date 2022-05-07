f3ea2d9dcc4617ec189d8e1f3274c964a9836455
Arek <arek.stachyra41@gmail.com>
2022-05-07 12:50:52 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    if (typeof(numStart) !== 'number' || typeof(numEnd) !== 'number') return 'ERROR';
+    if (numStart < 0 || numEnd < 0) return 'ERROR';
+    if (numEnd === numStart) return numEnd;
+    if (numStart > numEnd) {
+        [numStart, numEnd] = [numEnd, numStart]
+    }
 
+    // const nums = []
+    // for (let i = numEnd; i >= numStart; i--) {
+    //     nums.push(i);
+    // }
+
+    // let sum = 0;
+    // nums.forEach((num) => sum += num);
+
+    let sum = 0
+    for (let i = numStart; i <= numEnd; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// if numEnd smaller than numStart
+// just loop backwards until num End equals to numStart
+// sum the elements in function body
+// add those numbers into an array
