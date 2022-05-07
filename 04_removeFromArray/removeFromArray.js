3f9d78195eb294ec73d4aa6560705062aac234db
Arek <arek.stachyra41@gmail.com>
2022-05-07 11:46:30 +0200

    Complete excercises 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedElements) {
+    // for (const el of removedElements) {
+    //     if (!(array.includes(el))) continue
+    //     let index = array.indexOf(el);
+    //     array.splice(index,1)
+    // }
+    // return array;
+    return array.filter(el => !removedElements.includes(el))
 };
 
 // Do not edit below this line
