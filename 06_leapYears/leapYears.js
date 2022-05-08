41c725fdd49d7ff75653c6bc3a6c7886f69e2eb8
Arek <arek.stachyra41@gmail.com>
2022-05-08 14:40:20 +0200

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if ((year % 4 !== 0) && (year % 400 !== 0)) return false
+    // if ((year % 100 === 0) && (year % 400 !== 0)) return false
+    // return true
 
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 };
+
+
+// if year %% 4 === 0 -> true
+// dividisble by 100 - false
+// by 400 - true
 
 // Do not edit below this line
 module.exports = leapYears;
