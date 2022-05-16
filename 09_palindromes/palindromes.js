502a049059fcd366392762da208b86f75a162669
Arek <arek.stachyra41@gmail.com>
2022-05-16 13:34:36 +0200

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.toLowerCase().replace(/\s/g, "");
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    text = text.split('');
+    textFiltered = text.filter(letter => !punctuation.includes(letter)).join('');
+    textFilteredReversed = textFiltered.split('').reverse().join('');
+    if (textFiltered !== textFilteredReversed) return false;
+    return true;
 };
 
 // Do not edit below this line
