f30063c4b023148b98b10ba37a866d1afbea5957
Arek <arek.stachyra41@gmail.com>
2022-05-17 20:03:04 +0200

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+function fibonacci(num) {
+    if (num < 0) return 'OOPS';
+    // my solution
+	// const fib = [1,1];
+    // for (let i = 0; i < num-2; i++) {
+    //     fib.push(fib[i]+fib[i+1]);
+    // }
+    // return fib[num-1]
 
-};
+    // https://javascript.info/task/fibonacci-numbers
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= num; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
