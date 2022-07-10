1b30b999291684093304398974d7cdb0b1228c74
Arek <arek.stachyra41@gmail.com>
2022-07-10 23:42:20 +0200

    Change findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    if (!('yearOfDeath' in arr)) arr.yearOfDeath = new Date().getFullYear();
+    return arr.reduce((max, person) => (max.yearOfDeath - max.yearOfBirth) > (person.yearOfDeath - person.yearOfBirth) ? max : person, 0);
 };
+//https://stackoverflow.com/questions/36941115/return-object-with-highest-value
 
 // Do not edit below this line
 module.exports = findTheOldest;
